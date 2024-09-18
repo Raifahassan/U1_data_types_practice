a6d287c200f5225e3180904935d9a9066b464be7
raifa <nononono137@gmail.com>
2024-09-18 16:33:30 -0400

    completed

--- script.js
+++ script.js
@@ -46,33 +46,33 @@
  * Retrieve "Marty McFly" from each of the arrays below.
  */
 
-let array1 = ['Marty Marion', 'Marty Feldman', 'Marty McFly', 'Marty Marion'];
-let array2 = [
-  ['Marty Feldman', 'Marty Marion'],
-  ['Marty Stuart', 'Marty McFly'],
-  ['Marty Jannetty', 'Marty Robbins']
-];
-let array3 = [
-  ['Marty Feldman', ['Marty Marion']],
-  ['Marty Stuart', ['Marty Janetty', ['Marty McFly'], 'Marty Robbins']]
-];
+// let array1 = ['Marty Marion', 'Marty Feldman', 'Marty McFly', 'Marty Marion'];
+// let array2 = [
+//   ['Marty Feldman', 'Marty Marion'],
+//   ['Marty Stuart', 'Marty McFly'],
+//   ['Marty Jannetty', 'Marty Robbins']
+// ];
+// let array3 = [
+//   ['Marty Feldman', ['Marty Marion']],
+//   ['Marty Stuart', ['Marty Janetty', ['Marty McFly'], 'Marty Robbins']]
+// ];
 
 /*
  *
  * Use the length of the array below to retrieve the second to last item.
  */
 
-let array4 = ['a', 'b', 'c', 'd', 'e'];
+//let array4 = ['a', 'b', 'c', 'd', 'e'];
 
 /*
  *
  * Use the following arrays to answer the subprompts below.
  */
 
-let thom = ['Thom', 1000, 'Christchurch'];
-let karolin = ['Karolin', 16, 'New York'];
-let kristyn = ['Kristyn', 5, 'Pittsburgh'];
-let cathleen = ['Cathleen', 186, 'New York'];
+//let thom = ['Thom', 1000, 'Christchurch'];
+//let karolin = ['Karolin', 16, 'New York'];
+//let kristyn = ['Kristyn', 5, 'Pittsburgh'];
+//let cathleen = ['Cathleen', 186, 'New York'];
 
 // Cathleen decides that Thom can't be named "Thom" anymore. Remove "Thom" from
 // the thom array and replace it with "Tom".
@@ -83,3 +83,91 @@ let cathleen = ['Cathleen', 186, 'New York'];
 // Change Cathleen's hometown from New York to "Gotham City".
 
 // Remove "Pittsburgh" from Kristyn's array and add "Oakland".
+
+ 
+//let age=17
+//if (age<20 && age >=13) {
+ // console.log('you are a teenager')}
+ // else {console.log()}
+ // age >=18 ? console.log('you can vote'): console.log('you cant vote')
+
+  //switch statement are another conditional
+
+
+    //const food='fish';
+   //switch(food){
+    //case 'chicken':
+     // console.log('ill have the chicken francese');
+      //break;
+     // case 'steak':
+     // console.log('the steak please,medium rare ');
+    // break;
+   // else 'fish';
+   //console.log('i think ill try the salmon');
+   //break:
+   //fault:
+     
+  //}
+
+   for
+   (let i =0; i< 10; i++){
+    console.log(i)
+   }
+   for
+   (let i =10; i< 10; i+=20){
+   console.log(i)
+   }
+
+   const food=(' tacos ', 'burritos','pizza','bbq','sushi')
+   for(let i=0; i<= food.length; i++){
+    console.log('lets have${food[i]} for dinner tonight')
+
+   }
+   const wizards=[
+    {name:"Harry potter",house:"gryffindor"},
+    {name:"Lord Voldomort",house:"stytherin"},
+    {name:"Cedric Diggory",house:"hufflepuff"},
+    {luna:"Luna Lovegood",house:"ravenclaw"},
+    {name:"Albus Dumbledor",house:"gryffindor"},
+    {name:"Merlin ",house:"stytherin" },
+    {name:"pomona Sprout",house:"Hufflepuff"},
+    {name:"Gilderoy Lockheart",house:"Ravenclaw"},
+    {name:"Ron Weasley",house:"Gryffindor"},
+    {name:}
+
+
+   ]
+
+  for(let i=0; i< wizards.length; i++){
+    console.log(wizards[i].name)
+
+  }
+
+
+  //function sayHello(name){
+    //console.log("hello ${name} have a nice ${day},you have ${money}dollars in your account");
+
+  //}
+  //calling the function
+  //passing in an argument
+  //sayHello('jeremy','wednesday', 2000)
+  //sayHello("Brittany",5000,'Tuesday')
+  //sayHello("Ashley",1000,)
+  //sayHello("Dan JR",house:)
+
+
+
+  //function 
+ // const sayGoodbye= (name) =>  console.log("Goodbye ${name}")
+
+  //calling function
+ // sayGoodbye("name")
+
+
+ 
+ const Sum =(num1,num2)=>console.log(num1+num2)
+ const addNumber =(num1,num2)=>console.log(num1-num2)
+addNumber(5,4)
+addNumber(5,4)
+
+  
